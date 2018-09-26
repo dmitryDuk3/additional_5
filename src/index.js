80a4824da5b4694d8d428f96d0161997f549dd2a
Richard B <C:\Users\dmitr\AppData\Roaming\The Bat!>
2018-09-26 08:31:51 +0300

    I'm tired from this sh*t

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  
+  let array = str.split('');
+  for(let i = 0; i < bracketsConfig.length; i++) {
+   let open = 0;
+   let close = 0;
+    for(let k = 0; k < array.length; k++) {
+     if (array[k] === bracketsConfig[i][0]) {
+       
+       if(k + 1 < array.length) {
+         for(let j = 0; j < bracketsConfig.length; j++) {
+           if(str[k + 1] === bracketsConfig[j][1]) {
+             if(i === j) {
+               continue;
+             }
+              return false;
+           }
+         }
+       }
+       
+       open++;
+     }
+     
+     if (array[k] === bracketsConfig[i][1]) {
+       close++;
+     }
+      if(close > open) {
+       return false;
+     }
+   }
+    if(close !== open) {
+     return false;
+   }
+ }
+  return true;
 }
